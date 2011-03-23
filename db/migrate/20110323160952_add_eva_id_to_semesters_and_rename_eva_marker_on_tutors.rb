a9f9ba0f72635eb6b398cbf949e40f4100aad0b4
Alexander E. Fischer <aef@raxys.net>
2011-03-23 17:13:34 +0100

    Added eva_id to semesters and renamed eva_marker to eva_id on tutors

---
+++ db/migrate/20110323160952_add_eva_id_to_semesters_and_rename_eva_marker_on_tutors.rb
@@ -0,0 +1,11 @@
+class AddEvaIdToSemestersAndRenameEvaMarkerOnTutors < ActiveRecord::Migration
+  def self.up
+    add_column :semesters, :eva_marker, :string
+    rename_column :tutors, :eva_marker, :eva_id
+  end
+
+  def self.down
+    rename_column :tutors, :eva_id, :eva_marker
+    remove_column :semesters, :eva_marker, :string
+  end
+end
