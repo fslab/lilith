6bfc905ae6c6a8abb93b36c96076cdce29507290
Daniel Meißner <daniel@3st.mine.nu>
2011-03-24 22:40:41 +0100

    Funktion zum trennen des Titels vom Namen

--- lib/lilith/hbrs_tutor_scraper.rb
+++ lib/lilith/hbrs_tutor_scraper.rb
@@ -1,34 +1,51 @@
 # encoding: UTF-8
+
 require 'rubygems'
 require 'mechanize'
 require 'pp'
 
 class Lilith::HbrsTutorScraper
+  SECTION_LINK_PATTERN = "//p/a[@href and not(@href='javascript:history.back()')]"
 
   attr_accessor :agent
-  attr_accessor :title
-  attr_accessor :forename
-  attr_accessor :surname
-  attr_accessor :profil_url
   
-  def initialize
-    @agent = Mechanize.new
-    @url = "http://www.inf.h-bonn-rhein-sieg.de/Personen/ProfessorInnen.html"
+  def initialize (options = {})
+    @agent = options[:agent] || Mechanize.new
+    @url = options[:url] || "https://www.inf.h-bonn-rhein-sieg.de/Personen/ProfessorInnen.html"
   end
 
   def call
-    page = @agent.get(@url)
-    page.parser.xpath("//p/a[@href]").each do |link|
+    scrape_tutors
+  end
 
+  def scrape_tutors
+    page = @agent.get(@url)
+    page.parser.xpath(SECTION_LINK_PATTERN).each do |link|
+
+      puts split_up_title(link.text)
+      
       if link['href'].include? "http\:\/\/"
-        @profil_url = link['href']
+        puts link['href']
      else
        if link['href'].include? "\.html"
-         @profil_url = "http://www.inf.h-bonn-rhein-sieg.de" + link['href']
+         puts "http://www.inf.h-bonn-rhein-sieg.de" + link['href']
+         # puts link.text.scan(/Prof\.|Dr\.\-Ing|Dr\./)
        end
      end
 
-      @title = link.text.scan(/Prof\.|Dr\.\-Ing|Dr\./)
+      
     end
+  end
+
+  def split_up_title (title_and_name)
+    title_and_name.split(' ').each do |feld|
+      if feld.eql?("Prof.")|feld.eql?("Dr.")|feld.eql?("Dr.-Ing.")
+        if !title.empty?
+          title = title + " "
+        end
+      end
+      title = title + feld
+    end
+    title
   end
 end
