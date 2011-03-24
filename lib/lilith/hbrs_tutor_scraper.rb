e2f10e5f58996da68af8b2adf2136fffd244e31e
Daniel Meißner <daniel@3st.mine.nu>
2011-03-25 00:46:17 +0100

    noch etwas mit dem Aufsplitten von Titel, Name und Vorname gespielt

--- lib/lilith/hbrs_tutor_scraper.rb
+++ lib/lilith/hbrs_tutor_scraper.rb
@@ -22,14 +22,15 @@ class Lilith::HbrsTutorScraper
     page = @agent.get(@url)
     page.parser.xpath(SECTION_LINK_PATTERN).each do |link|
 
-      puts split_up_title(link.text)
+      title = split_up_title(link.text)
+      forename = split_up_forename(link.text)
+      surname = split_up_surname(link.text)
       
       if link['href'].include? "http\:\/\/"
         puts link['href']
      else
        if link['href'].include? "\.html"
          puts "http://www.inf.h-bonn-rhein-sieg.de" + link['href']
-         # puts link.text.scan(/Prof\.|Dr\.\-Ing|Dr\./)
        end
      end
 
@@ -37,15 +38,30 @@ class Lilith::HbrsTutorScraper
     end
   end
 
+  #Titel
   def split_up_title (title_and_name)
+    title = ""
     title_and_name.split(' ').each do |feld|
       if feld.eql?("Prof.")|feld.eql?("Dr.")|feld.eql?("Dr.-Ing.")
-        if !title.empty?
-          title = title + " "
+        if title.empty?
+          title = feld
+        else
+          title = title + " " + feld
         end
       end
-      title = title + feld
     end
     title
+  end
+
+  #Vorname
+  def split_up_forename (title_and_name)
+    #TODO: Zweite Vornamen und Adelsnamen werden nicht beachtet
+    array = split_up_title(title_and_name).split(' ')
+    forename = title_and_name.split(' ').at(array.size)
+  end
+
+  #Nachname
+  def split_up_surname (title_and_name)
+    surname = title_and_name.split(' ').last
   end
 end
