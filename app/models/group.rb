d30a7509ac8851289ca5508791c905710aafb8eb
Alexander E. Fischer <aef@raxys.net>
2011-03-27 03:55:20 +0200

    Added missing association of Group to Course

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 # One partition of the participants of a course
 class Group < ActiveRecord::Base
+  belongs_to :course
+
   has_many :event_associations,
            :class_name => 'EventGroupAssociation',
            :dependent => :destroy
