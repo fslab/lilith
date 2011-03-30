a84c6701874151a785fa2266be85778d63987f7f
Alexander E. Fischer <aef@raxys.net>
2011-03-30 21:16:46 +0200

    Fixed backward migration error

--- db/migrate/20110330160845_change_begin_year_on_semesters.rb
+++ db/migrate/20110330160845_change_begin_year_on_semesters.rb
@@ -1,11 +1,11 @@
 class ChangeBeginYearOnSemesters < ActiveRecord::Migration
   def self.up
-    remove_column(:semesters, :begin_year)
-    add_column(:semesters, :start_year, :integer)
+    remove_column :semesters, :begin_year
+    add_column :semesters, :start_year, :integer
   end
 
   def self.down
-    remove_column(:semesters, :start_year, :integer)
-    add_column(:semesters, :begin_year, :date)
+    remove_column :semesters, :start_year
+    add_column :semesters, :begin_year, :date
   end
 end
