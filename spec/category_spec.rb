7e1f74a6b0c0060efdc3e848986a2640cca9f594
Alexander E. Fischer <aef@raxys.net>
2011-03-30 18:45:19 +0200

    Fixed plan an spec by removing token and using eva_id as intended again

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Category do
   it { should have_db_column(:name).of_type(:string) }
-  it { should have_db_column(:token).of_type(:string) }
+  it { should have_db_column(:eva_id).of_type(:string) }
   it { should have_db_column(:created_at).of_type(:datetime) }
   it { should have_db_column(:updated_at).of_type(:datetime) }
 
