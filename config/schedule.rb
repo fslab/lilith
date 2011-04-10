ab9b5e3685f7c5dd62e91e38fe97564ef8a3e1fb
Daniel Meißner <daniel@3st.mine.nu>
2011-04-11 00:14:14 +0200

    add cronjob for eva scraper

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,22 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+=begin
+Copyright Alexander E. Fischer <aef@raxys.net>, 2011
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+This file is part of Lilith.
 
-# Learn more: http://github.com/javan/whenever
+Lilith is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lilith is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lilith.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+every :hour do
+  runner 'Lilith::scrape_eva', :output => 'cron.log', :environment => :development
+end
