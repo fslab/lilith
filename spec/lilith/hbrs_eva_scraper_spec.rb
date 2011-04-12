f60d93ac7e610a876aadfd8317e7b0ef0668ebf6
Alexander E. Fischer <aef@raxys.net>
2011-04-12 23:16:09 +0200

    Scraper now associates weeks and events. Still buggy

--- spec/lilith/hbrs_eva_scraper_spec.rb
+++ spec/lilith/hbrs_eva_scraper_spec.rb
@@ -1,4 +1,4 @@
-=begin
+=begin encoding: UTF-8
 Copyright Alexander E. Fischer <aef@raxys.net>, 2011
 
 This file is part of Lilith.
@@ -35,6 +35,76 @@ describe Lilith::HbrsEvaScraper do
   it "should find 16 study units in the fixture file" do
     @scraper.agent = Lilith::MockAgent.new(:mock_document_file => MENU_FILE)
 
-    @scraper.scrape_study_units.should have(16).items
+    semester = Semester.make(:start_week => '2011-W12', :start_week => '2011-W25')
+
+    @scraper.scrape_study_units(semester).should have(16).items
+  end
+
+  context ".parse_week_range" do
+    it "should be able to parse '13+14'" do
+      semester = Semester.make(:start_week => '2011-W10', :end_week => '2011-W24')
+
+      expected_weeks = [13, 14].map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, '13+14').should == expected_weeks
+    end
+
+    it "should be able to parse '13 + 14 '" do
+      semester = Semester.make(:start_week => '2011-W10', :end_week => '2011-W24')
+
+      expected_weeks = [13, 14].map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, '13 + 14 ').should == expected_weeks
+    end
+
+    it "should be able to parse '12-25'" do
+      semester = Semester.make(:start_week => '2011-W12', :end_week => '2011-W25')
+      
+      expected_weeks = (12..25).map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, '12-25').should == expected_weeks
+    end
+
+    it "should be able to parse ' 12 - 25'" do
+      semester = Semester.make(:start_week => '2011-W12', :end_week => '2011-W25')
+
+      expected_weeks = (12..25).map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, ' 12 - 25').should == expected_weeks
+    end
+
+    it "should be able to parse '12,14,15,17'" do
+      semester = Semester.make(:start_week => '2011-W10', :end_week => '2011-W24')
+
+      expected_weeks = [12, 14, 15, 17].map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, '12,14,15,17').should == expected_weeks
+    end
+
+    it "should be able to parse '12, 14,15 , 17'" do
+      semester = Semester.make(:start_week => '2011-W10', :end_week => '2011-W24')
+      
+      expected_weeks = [12, 14, 15, 17].map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, '12, 14,15 , 17').should == expected_weeks
+    end
+
+    it "should be able to parse '49-51,1+3'" do
+      semester = Semester.make(:start_week => '2011-W44', :end_week => '2012-W06')
+
+      expected_weeks = []
+      expected_weeks += [49, 50, 51].map{|index| Lilith::Week.new(2011, index)}
+      expected_weeks += [1, 3].map{|index| Lilith::Week.new(2012, index)}
+
+      described_class.parse_week_range(semester, '49-51,1+3').should == expected_weeks
+    end
+
+    it "should be able to parse 'ab KW 15'" do
+      semester = Semester.make(:start_week => '2011-W12', :end_week => '2011-W25')
+
+      expected_weeks = (15..25).map{|index| Lilith::Week.new(2011, index)}
+
+      described_class.parse_week_range(semester, 'ab KW 15').should == expected_weeks
+    end
   end
 end
