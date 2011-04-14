3d571deafbf9e8fe85b57b0297a134fb7441ff25
Alexander E. Fischer <aef@raxys.net>
2011-04-15 01:44:08 +0200

    Changed timezone, locale and schema format configurations

--- config/application.rb
+++ config/application.rb
@@ -43,11 +43,11 @@ module Lilith
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
@@ -61,5 +61,7 @@ module Lilith
     config.generators do |g|
       g.fixture_replacement :machinist
     end
+
+    config.active_record.schema_format = :sql
   end
 end
