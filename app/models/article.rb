a00386e1df3a8b9f7657e323b7f6482c2c49cddd
Alexander E. Fischer <aef@raxys.net>
2011-04-28 21:55:51 +0200

    Fixed Article#published virtual attribute, implemented default sort order and filter scopes. Wrote tests.

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,14 +8,38 @@ class Article < ActiveRecord::Base
   validates :name, :presence => true
   validates :body, :presence => true
 
-  def published
-    @published || !!self.published_at
+  default_scope order('published_at DESC, updated_at DESC')
+
+  def self.sticky
+    where(:sticky => true)
   end
+
+  def self.non_sticky
+    where(:sticky => false)
+  end
+
+  def self.published
+    where('published_at IS NOT NULL')
+  end
+
+  def self.unpublished
+    where('published_at IS NULL')
+  end
+
+  def published
+    if @published == true or @published == false
+      @published
+    else
+      !!self.published_at
+    end
+  end
+
+  alias published? published
 
   protected
 
   def set_published_at
-    if published
+    if published?
       self.published_at = Time.now unless self.published_at
     else
       self.published_at = nil
--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -1,9 +1,144 @@
 require 'spec_helper'
 
-
-describe Article do 
+describe Article do
+  it { should have_db_column(:id) }
   it { should have_db_column(:name).of_type(:string) }
   it { should have_db_column(:published_at).of_type(:datetime) }
   it { should have_db_column(:abstract).of_type(:text) }
   it { should have_db_column(:body).of_type(:text) }
+
+  it_should_behave_like "a timestamped model"
+
+  context "default order" do
+    it "should order by published_at in a descending way" do
+      latest_article = described_class.make!(:published_at => Time.new(2011, 4, 28, 14), :updated_at => nil)
+      older_article  = described_class.make!(:published_at => Time.new(2011, 4, 28, 10), :updated_at => nil)
+
+      articles = described_class.all
+      articles[0].should == latest_article
+      articles[1].should == older_article
+    end
+
+    it "should order by updated_at in a descending way" do
+      latest_article = described_class.make!(:updated_at => Time.new(2011, 4, 28, 14), :published_at => nil)
+      older_article  = described_class.make!(:updated_at => Time.new(2011, 4, 28, 10), :published_at => nil)
+
+      articles = described_class.all
+      articles[0].should == latest_article
+      articles[1].should == older_article
+    end
+
+    it "should prefer published_at before updated_at" do
+      latest_article = described_class.make!(:published_at => Time.new(2011, 4, 28, 14), :updated_at => Time.new(2011, 4, 28, 10))
+      older_article  = described_class.make!(:published_at => Time.new(2011, 4, 28, 10), :updated_at => Time.new(2011, 4, 28, 14))
+
+      articles = described_class.all
+      articles[0].should == latest_article
+      articles[1].should == older_article
+    end
+  end
+
+  context "sticky filters" do
+    it "should allow to receive only sticky articles" do
+      3.times do
+        described_class.make!(:sticky => true)
+        described_class.make!(:sticky => false)
+      end
+
+      described_class.sticky.all?{|article| article.sticky? }
+    end
+
+    it "should allow to receive only non-sticky articles" do
+      3.times do
+        described_class.make!(:sticky => true)
+        described_class.make!(:sticky => false)
+      end
+
+      described_class.non_sticky.all?{|article| not article.sticky? }
+    end
+  end
+
+  context "published filters" do
+    it "should allow to receive only published articles" do
+      3.times do
+        described_class.make!(:published_at => Time.now)
+        described_class.make!(:published_at => nil)
+      end
+
+      described_class.published.all?{|article| article.published? }
+    end
+
+    it "should allow to receive only unpublished articles" do
+      3.times do
+        described_class.make!(:published_at => Time.now)
+        described_class.make!(:published_at => nil)
+      end
+
+      described_class.unpublished.all?{|article| not article.published? }
+    end
+  end
+
+  context "#published? (virtual attribute)" do
+    it "should return true if published_at is set" do
+      article = described_class.make(:published_at => Time.now)
+
+      article.should be_published
+    end
+
+    it "should return false if overridden even if published_at is set" do
+      article = described_class.make(:published_at => Time.now)
+      article.published = false
+      article.should_not be_published
+    end
+
+    it "should return false if published_at is not set" do
+      article = described_class.make(:published_at => nil)
+
+      article.should_not be_published
+    end
+
+    it "should return true if overridden even if published_at is not set" do
+      article = described_class.make(:published_at => nil)
+      article.published = true
+      article.should be_published
+    end
+  end
+
+  context "#published= (virtual attribute)" do
+    it "if set to true, save should set published_at if it is unset" do
+      article = described_class.make(:published_at => nil)
+
+      lambda {
+        article.published = true
+        article.save!
+      }.should change{ article.published_at }.from(nil)
+    end
+
+    it "if set to true, save should not change published_at if it is already set" do
+      article = described_class.make(:published_at => Time.now)
+
+      lambda {
+        article.published = true
+        article.save!
+      }.should_not change{ article.published_at }
+    end
+
+    it "if set to false, save should set published_at to nil if it is already set" do
+      article = described_class.make(:published_at => Time.now)
+
+      lambda {
+        article.published = false
+        article.save!
+      }.should change{ article.published_at }.to(nil)
+    end
+
+    it "if set to false, save should not change published_at if it is unset" do
+      article = described_class.make(:published_at => nil)
+
+      lambda {
+        article.published = false
+        article.save!
+      }.should_not change{ article.published_at }
+    end
+  end
 end
