122f979cb68a7ce609a1ec098a13e06e138c196e
Alexander E. Fischer <aef@raxys.net>
2011-04-29 01:28:30 +0200

    Implemented article translations using globalize3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,50 +1,59 @@
+# A single entry of the news feed
 class Article < ActiveRecord::Base
   include Lilith::UUIDHelper
 
-#  def self.translates(*attributes)
-#    puret(*attributes)
-#
-#    TRANSLATED_ATTRIBUTES.each do |attribute|
-#      I18n.available_locales.each do |locale|
-#        define_method("#{attribute}_#{locale}=") do |name|
-#          puret_attributes[locale][attribute] = name
-#        end
-#
-#        define_method("#{attribute}_#{locale}") do
-#          puret_attributes[locale][attribute]
-#        end
-#      end
-#    end
-#  end
-
   translates :name, :abstract, :body
 
-  attr_accessor :published
+  attr_writer :published
 
   before_save :set_published_at
 
   validates :name, :presence => true
   validates :body, :presence => true
 
+  # Default order for articles
   default_scope order('published_at DESC, updated_at DESC')
 
+  # Selects only sticky articles
   def self.sticky
     where(:sticky => true)
   end
 
+  # Selects only non-sticky articles
   def self.non_sticky
     where(:sticky => false)
   end
 
+  # Selects only published articles
   def self.published
     where('published_at IS NOT NULL')
   end
 
+  # Selects only unpublished articles
   def self.unpublished
     where('published_at IS NULL')
   end
 
-  def published
+  # Define reader and writer methods for supported locales
+  #
+  # Example:
+  #
+  #   article.name_en = 'The title'
+  #   article.body_de
+  translated_attribute_names.each do |attribute|
+    I18n.available_locales.each do |locale|
+      define_method "#{attribute}_#{locale}" do
+        read_attribute(attribute, :locale => locale)
+      end
+
+      define_method "#{attribute}_#{locale}=" do |value|
+        write_attribute(attribute, value, :locale => locale)
+      end
+    end
+  end
+
+  # Virtual attribute reflecting if the article is currently published
+  def published?
     if @published == true or @published == false
       @published
     else
@@ -52,10 +61,10 @@ class Article < ActiveRecord::Base
     end
   end
 
-  alias published? published
-
   protected
 
+  # If published? is true, set the published_at timestamp if not already set,
+  # if published? is false, keep or set published_at timestamp to nil
   def set_published_at
     if published?
       self.published_at = Time.now unless self.published_at
--- app/models/article/translation.rb
+++ app/models/article/translation.rb
@@ -1,3 +1,6 @@
+# Contains translations for Article
 class Article::Translation < Globalize::ActiveRecord::Translation
+  set_table_name 'article_translations'
+  
   include Lilith::UUIDHelper
 end
