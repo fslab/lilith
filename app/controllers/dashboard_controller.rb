d05b74bb8992899e15f0dee4028ab7b3f45803cb
Alexander E. Fischer <aef@raxys.net>
2011-04-29 02:28:17 +0200

    Work on article and dashboard system

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
 
   def show
+    @sticky_articles = Article.published.sticky.limit(10)
 
+    @articles = Article.published.non_sticky.limit(10)
   end
 
 end
