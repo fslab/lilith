30c9ca4857596a0ece92ced6ab077bded3441e97
Alexander E. Fischer <aef@raxys.net>
2011-04-30 03:14:33 +0200

    Found out that HAML can inline textile. Removed textile template handler and converted templates to HAML with inline textile

--- lib/lilith/textile_template_handler.rb
+++
@@ -1,7 +0,0 @@
-class Lilith::TextileTemplateHandler < ActionView::Template::Handler
-  def self.call(template)
-    "'#{RedCloth.new(template.source).to_html}'"
-  end
-end
-
-ActionView::Template.register_template_handler(:textile, Lilith::TextileTemplateHandler)
