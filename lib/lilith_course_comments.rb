13ede35649f9048afaf656040e0c40998c42eedc
Wurmfort PPU Desktop 2 <aef+wurmfort-ppu2@raxys.net>
2011-10-09 23:52:23 +0200

    Missed some parts while renaming

--- lib/lilith_course_comments.rb
+++ lib/lilith_course_comments.rb
@@ -1,4 +1,4 @@
-module LilithTest
+module LilithCourseComments
   class Engine < Rails::Engine
     initializer :add_navigation_configuration do |app|
       load (paths.path + 'config' + 'navigation.rb').to_s
